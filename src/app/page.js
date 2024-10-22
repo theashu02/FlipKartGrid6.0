046b90c303343ca8916747c43372dd0b97d5f1e8
theashu02 <ashutoshchauhan1919@gmail.com>
2024-10-23 03:08:45 +0530

    loader and ui

--- src/app/page.js
+++ src/app/page.js
@@ -5,8 +5,24 @@ import Features from "./Components/Features";
 import Footer from "./Components/Footer";
 import ProductDescription from "./Components/ProductDescription";
 import FreshnessDetection from "./Components/FreshnessDetection";
+import LoadingPage from "./Components/LoadingPage";
+import { useState,useEffect } from "react";
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading delay
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000); // Change to your desired loading time
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <LoadingPage />; // Show loading component while loading
+  }
   return (
     <div className="min-h-screen bg-gray-50">
       <Navbar />
---
+++ src/pages/ContactUs.jsx
@@ -0,0 +1,14 @@
+import ContactPage from '@/app/Components/ContactPage'
+import '../app/globals.css'
+
+
+export default function ContactUs() {
+    return (
+        <> 
+         
+          <ContactPage />
+        
+            
+        </>
+    )
+}
